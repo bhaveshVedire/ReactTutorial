1597697ffc0b24c8f47e9df0ecffc6a3ae3593bd
Vedire Bhavesh <bhaveshredychinna@gmail.com>
2020-04-02 02:30:49 +0530

    first

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+ReactDOM.render(<h1>Hi ra Bhavesh</h1>, document.getElementById('test'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
